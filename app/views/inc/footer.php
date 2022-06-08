e7d37dea29e08de57ba22a176f2c82b399e744f4
hasnaoui-said <saidhasnaoui.uce@gmail.com>
2022-06-08 11:52:28 +0100

    another theme 2 admin

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,13 +1,28 @@
-	</div>
-	<!-- END #app -->
+  <!-- jquery-->
+  <script src="<?php echo URLROOT; ?>/public/js/jquery-3.3.1.min.js"></script>
+    <!-- Plugins js -->
+    <script src="<?php echo URLROOT; ?>/public/js/plugins.js"></script>
+    <!-- Popper js -->
+    <script src="<?php echo URLROOT; ?>/public/js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="<?php echo URLROOT; ?>/public/js/bootstrap.min.js"></script>
+    <!-- Counterup Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/jquery.counterup.min.js"></script>
+    <!-- Moment Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/moment.min.js"></script>
+    <!-- Waypoints Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/jquery.waypoints.min.js"></script>
+    <!-- Scroll Up Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/jquery.scrollUp.min.js"></script>
+    <!-- Full Calender Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/fullcalendar.min.js"></script>
+    <!-- Chart Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/Chart.min.js"></script>
+    <!-- Custom Js -->
+    <script src="<?php echo URLROOT; ?>/public/js/main.js"></script>
 
-	<!-- ================== BEGIN core-js ================== -->
-	<script src="<?php echo URLROOT; ?>/public/assets/js/vendor.min.js"></script>
-	<script src="<?php echo URLROOT; ?>/public/assets/js/app.min.js"></script>
-	<!-- ================== END core-js ================== -->
-
-	<!-- <script src="<?php echo URLROOT; ?>/public/assets/js/demo/dashboard.js"></script> -->
-	<!-- ================== END page-js ================== -->
 </body>
 
+
+<!-- Mirrored from www.radiustheme.com/demo/html/psdboss/akkhor/akkhor/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 08 Jun 2022 09:18:40 GMT -->
 </html>
